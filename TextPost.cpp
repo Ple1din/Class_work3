0e53c8846941730088c8d31261c844df8f962dc8
LuisHorca <a01713551@tec.mx>
2025-05-22 18:32:49 -0600

    TextPost.cpp

--- TextPost.cpp
+++ TextPost.cpp
@@ -1,9 +1,1 @@
-#include "TextPost.h"
-
-TextPost::TextPost(const std::string& author, const std::string& date, const std::string& content)
-    : Post(author, date, content) {}
-
-std::string TextPost::display() const {
-    return "---Text Post---\n" + Post::display();
-}
 
