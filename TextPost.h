8d0bf0f4cd3fae3494d1530624a50b6741117289
LuisHorca <a01713551@tec.mx>
2025-05-22 18:32:08 -0600

    TextPost.h

--- TextPost.h
+++ TextPost.h
@@ -1,1 +1,13 @@
+#ifndef TEXTPOST_H
+#define TEXTPOST_H
 
+#include "Post.h"
+
+class TextPost : public Post {
+public:
+    TextPost(const std::string& author, const std::string& date, const std::string& content);
+
+    std::string display() const override;
+};
+
+#endif
